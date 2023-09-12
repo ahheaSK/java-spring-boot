aebcd1e3f96874d02652efdb3e6a498032c5c980
DESKTOP-NFBVRP4\Lenovo <doemmakarask@gmail.com>
2023-09-12 15:22:38 +0700

    first commit

---
+++ src/main/java/com/java/makara/gmail/Phone/Shop/Night/Java0/PhoneShopNightJava20Application.java
@@ -0,0 +1,13 @@
+package com.java.makara.gmail.Phone.Shop.Night.Java0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneShopNightJava20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneShopNightJava20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/makara/gmail/Phone/Shop/Night/Java0/PhoneShopNightJava20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.makara.gmail.Phone.Shop.Night.Java0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneShopNightJava20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
